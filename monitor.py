490ad1db1857927fbc75967d46472b6b93f097f8
root <feng4206yu@163.com>
2020-11-13 17:14:35 +0800

    upload sign

--- monitor.py
+++ monitor.py
@@ -8,6 +8,129 @@ import sys
 import os
 import socket
 import subprocess
+import hashlib
+import json
+import bz2
+import base64
+
+def upload_sign(pub_args, i_type, host, port):
+
+    timestamp = int(time.time())
+    pub_args["data"]["$timestamp"] = timestamp
+    print(repr(pub_args))
+    last_i = timestamp % 10
+    i_tmp = int(timestamp / 10)
+    i_tmp = int(i_tmp / 10)
+    rev_third_i = i_tmp % 10
+    print(last_i, rev_third_i)
+
+    data = pub_args["data"]
+    s_keys = sorted(data)
+    check_str = str(rev_third_i)
+    for k in s_keys:
+        check_str = check_str + "%s=%s" % (k, data[k])
+    check_str = check_str + str(last_i)
+    print(check_str)
+    data["$sign"] = hashlib.md5(check_str.encode("utf-8")).hexdigest()
+    print(repr(pub_args))
+
+    json_str = json.dumps(pub_args)
+    json_str_len = len(json_str)
+    data_len_4bytes = json_str_len.to_bytes(4, 'big')
+    type_4bytes = i_type.to_bytes(4, 'big')
+    
+    addr = (host, port)
+    t_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    t_sock.connect(addr)
+    t_sock.send(data_len_4bytes)
+    t_sock.send(type_4bytes)
+    t_sock.send(bytes(json_str.encode('utf-8')))
+    msg = t_sock.recv(128)
+    print(msg.decode('utf-8'))
+    print(len(msg))
+    returned_json_str = msg.decode('utf-8')
+    ind = returned_json_str.find('{')
+    json_str = returned_json_str[ind:]
+    print(json_str)
+    obj = json.loads(json_str)
+    print(obj["msg"])
+    encoding(t_sock)
+
+def encoding(arg_sock):
+    
+    pub = {}
+    pub["cmd"] = "/log/report/driver"
+
+    crash_data = {}
+    crash_data["$timestamp"] = 1605251401
+    crash_data["$log_type"] = "crash"
+    crash_data["area"] = "hb4"
+    crash_data["machine_name"] = "127.0.0.1"
+
+    json_str = json.dumps(crash_data)
+    json_str = json_str.replace(' ','')
+    print(json_str)
+    json_str = bz2.compress(json_str.encode('utf-8'))
+    b64 = base64.b64encode(json_str)
+    print(b64.decode('utf-8'))
+
+    log = {}
+    log['log'] = b64.decode('utf-8')
+    pub['data'] = log
+
+    json_str = json.dumps(pub)
+    json_str_len = len(json_str)
+    print(json_str_len)
+    data_len_4bytes = json_str_len.to_bytes(4, 'big')
+    type_4bytes = (1).to_bytes(4, 'big')
+    
+    arg_sock.send(data_len_4bytes)
+    arg_sock.send(type_4bytes)
+    arg_sock.send(bytes(json_str.encode('utf-8')))
+    msg = arg_sock.recv(128)
+    arg_sock.close()
+    print(msg.decode('utf-8'))
+
+
+def upload_crash(area, machine_name, i_type, host, port):
+
+    
+    pub = {}
+    pub["cmd"] = "log/report/driver"
+
+    crash_data = {}
+    crash_data["$timestamp"] = int(time.time())
+    crash_data["$log_type"] = "crash"
+    crash_data["area"] = "hb4"
+    crash_data["machine_name"] = "127.0.0.1"
+    print(repr(crash_data))
+    raw_json_str = json.dumps(crash_data)
+    raw_json_str = raw_json_str.replace(' ', '')
+    c = bz2.compress(raw_json_str.encode('utf-8'))
+    b64 = base64.b64encode(c)
+    compressed = {}
+    compressed["log"] = b64.decode('utf-8')
+    pub['data'] = compressed
+    print(repr(pub)) 
+    json_str = json.dumps(pub)
+    json_str_len = len(json_str)
+    print(json_str_len)
+    data_len_4bytes = json_str_len.to_bytes(4, 'big')
+    type_4bytes = i_type.to_bytes(4, 'big')
+    
+    addr = (host, port)
+    t_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    t_sock.connect(addr)
+    t_sock.send(data_len_4bytes)
+    t_sock.send(type_4bytes)
+    t_sock.send(bytes(json_str.encode('utf-8')))
+    #t_sock.shutdown(socket.SHUT_WR)
+    msg = t_sock.recv(128)
+    t_sock.close()
+    print(msg.decode('utf-8'))
+    print(len(msg))
+
+
 
 
 
@@ -23,6 +146,21 @@ def read_conf(conf_file):
     args['host'] = g['remote']['host']
     args['port'] = int(g['remote']['port'])
     return args
+
+def read_pub(pub_file):
+    pub = {}
+    data = {}
+    conf = configparser.ConfigParser()
+    conf.read(pub_file)
+    pub["cmd"] = conf["pub"]["cmd"]
+    data["$who"] = conf["data"]["$who"]
+    data["$debug"] = int(conf["data"]["$debug"])
+    data["$driver_name"] = conf["data"]["$driver_name"]
+    data["$wan_ip"] = conf["data"]["$wan_ip"]
+    data["$lan_ip"] = conf["data"]["$lan_ip"]
+    pub["data"] = data
+    return pub
+    
 
 def read_ip_list(ip_list_file):
     
@@ -98,6 +236,10 @@ class msgSender (threading.Thread):
 
 args = read_conf("./monitor.conf")
 ip_all = read_ip_list("./monitor.ip_list")
+pub = read_pub("./monitor.pub")
+upload_sign(pub, 1, args['host'], args['port'])
+#upload_crash('hb4', '192.168.2.241', 2, args['host'], args['port'])
+sys.exit(1)
 
 lock = threading.Lock()
 Q = queue.Queue(0)
